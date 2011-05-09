4f80df9bc0e784cc840d8623d74e0647e9a921b7
Chetan Bademi <chetan@chetan.(none)>
2011-05-09 21:06:35 +0530

    Wrote the Tag, Events, TabImage, TabForum, TabForumReply. Committer: Chetan Bademi <chetanbademi@gmail.com>

--- events/models.py
+++ events/models.py
@@ -1,91 +1,107 @@
-# have written a very basic crude definition of events_all model
-
-
-
-
 from django.db import models
-from django.contrib import admin
-from django import forms
-from main_test.users.models import *
 
-# Create your models here.
 
-class Tag(models.Model):
+
+# Author: Chetan Bademi - Wrote the initial model
+class Tag(models.Model):   
+#E.g.: aerofest, coding etc
     name=models.CharField(max_length=30)
     def __str__(self):
         return self.name
-    
     class Admin:
         pass
-
-class image_for_tabs(models.Model):
-    image=models.ImageField(upload_to="public_html/2011/media/") #how to avoid file conflicts ? Files with same name can't be stored in same directory
     
-class Tabs(models.Model):
-    content=models.TextField()
-    tabimage=models.ManyToManyField(image_for_tabs)  #One tab may have multiple images. We don't know how many
-    #Have yet to write questions and discussion forums classes 
-
-  
-         
-class event(models.Model):
-    
-    name = models.CharField(max_length=100)
+# Author: Chetan Bademi - Wrote the initial model
+class Event(models.Model):
+    name = models.CharField(max_length=80)
     url = models.URLField(null=True,verify_exists=False, blank=True)
+    tags=models.ManyToManyField(Tag, blank=True, null=True)
+    
+    # Registration start and end time
     start_time = models.DateTimeField(null=True,blank=True)
     end_time = models.DateTimeField(null=True,blank=True)
-    registerable = models.BooleanField()
-    users = models.ManyToManyField(generic_user, related_name='events', blank=True, null=True)
-    chosen_users = models.ManyToManyField(generic_user, blank=True, null=True, related_name='qualified_events')
-    flagged_by = models.ManyToManyField(generic_user,blank=True, null= True, related_name='flagged_events')
-    logo = models.FileField(upload_to="logos/", blank=True, null=True)
-    event_type = models.TextField(max_length=30) #Could be bool field also
-    tabs = models.ForeignKey(Tabs)
-    #Many to Many or foreign key ? I dunno
-    # OR tabs=models.XMLField()
-    #We could define tabs as an XML field in event itself. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
+    coords = models.ManyToManyField(User, blank=True, null=True, related_name='coord_events')
     
+    # Registration
+    registrable = models.BooleanField(default=False)
+    users = models.ManyToManyField(User,  blank=True, null=True, related_name='users_events')
+    chosen_users = models.ManyToManyField(User, blank=True, null=True, related_name='qualified_events')
+    
+    # Hospitality
+    accommodation = models.BooleanField(default=False)
+    
+    # MyShaastra 
+    flagged_by = models.ManyToManyField(User,  blank=True, null=True, related_name='flagged_events')
+    
+    # Logo and Sponsorship logos
+    #NOTE: Rename the uploaded image file to event name.
+    #NOTE: Assumption: There's one logo and one spons logo for each event
+    # Is this the correct path? CHECK THIS!
+    logo=models.FileField(upload_to="public_html/2011/events_logos/", blank=True, null=True)
+    sponslogo=models.FileField(upload_to="public_html/2011/events_sponslogos/", blank=True, null=True)
+    
+    # Tabs
+    tabs = models.ManyToManyField(Tabs, blank=True, null=True, related_name='tabs')
+  
     def __str__(self):
         return self.name
-
+        
     class Admin:
         pass
-#This is just last years code for questions. Need to think of ways to make it better    
-class Question(models.Model):
-    TYPE = (
-            (1, 'Normal'),
-            (2, 'File'),
-            (3, 'MCQ'),
-            #(4, 'Message'),
-            )
-    
-    event = models.ForeignKey(event)
-    content = models.TextField(max_length=1000)
-    pref_id = models.IntegerField(blank= False, null = False ,verbose_name = 'Question Number. Note that this will be the number displayed in the button for this question.',)
-    type = models.IntegerField(choices=TYPE)
-    visible = models.BooleanField(default=True)
+
+
+
+# Author: Chetan Bademi - Wrote the initial model
+def Tabs(models.Model): 
+    # NOTE: Will one text field per tab suffice?
+    text        = models.CharField(max_length=10000)
+    images      = models.ManyToManyField(TabImage      , blank=True, null=True, related_name='questions')
+    questions   = models.ManyToManyField(TabQuestion   , blank=True, null=True, related_name='questions')
+    forums      = models.ManyToManyField(TabForum      , blank=True, null=True, related_name='forums')
+  
     def __str__(self):
-        return self.content
-    def get_choices (self):
-        if self.type != 3:
-            raise ValueError
-        list = MCQOption.objects.filter(question=self)
-        choices = []
-        num = 1
-        for option in list:
-            choices.append ((num, option.content))
-            num+=1
+        return self.name
+    class Admin:
+        pass
 
-        return tuple(choices)
 
+
+# Author: Chetan Bademi - Wrote the initial model
+def TabImage(models.Model):
+    # TASK: Each tab can have more than one image. Each tab can be associated with more than one TabImage object(s)
+    # Rename the image file to the id of the TabImage object. 
+    # Ex: When a user wants to upload a image file, a TabImage object is created. Say it's id is 44. 
+    # Then rename the file to 44.jpg and store it in "public_html/2011/TabImage/"
+    image = models.ImageField(upload_to='public_html/2011/TabImage/')
+    def __str__(self):
+        return self.name
     class Admin:
         pass
     
-    class Meta:
-	ordering = ['pref_id','id',]      
+
+# Author: Chetan Bademi - Wrote the initial model
+def TabForum(models.Model):
+    name = models.CharField( max_length = 30 )
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    started_by = models.ForeignKey(User,blank=True, null=True, related_name='started_by')
+    time_created = models.DateTimeField(auto_now=False, auto_now_add=False)
+    time_modified = models.DateTimeField(auto_now=False, auto_now_add=False)
+    replies = models.ManyToManyField(TabForumReply,blank=True,null=True,related_name='replies')
+    def __str__(self):
+        return self.name
+    class Admin:
+        pass
 
 
-
-
-
-
+# Author: Chetan Bademi - Wrote the initial model
+def TabForumReply(models.Model):
+    reply_by = models.ForeignKey(User,blank=True, null=True, related_name='reply_by')
+    time_stamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    content = models.CharField( max_length = 3000 ) 
+    # Id of the TabForumReply object to which the user replied to.
+    # Using this we can provide link to the post to which this was a reply. 
+    reply_to = models.IntegerField()
+    # Number of likes and dislikes for a post. 
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    # NOTE: Do we need edit history? I don't think it's worth implementing this feature. 
