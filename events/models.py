53f053089eb4c570fe6a881728eece5e0a0fa880
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-09 16:01:47 +0530

    Added more comments

--- events/models.py
+++ events/models.py
@@ -23,7 +23,9 @@ class image_for_tabs(models.Model):
     
 class Tabs(models.Model):
     content=models.TextField()
-    tabimage=models.ManyToManyField(image_for_tabs)  #One tab may have multiple images. We don't know how many      
+    tabimage=models.ManyToManyField(image_for_tabs)  #One tab may have multiple images. We don't know how many
+    #Have yet to write questions and discussion forums classes 
+         
 class event(models.Model):
     
     name = models.CharField(max_length=100)
@@ -33,8 +35,10 @@ class event(models.Model):
     registerable = models.BooleanField()
     users = models.ManyToManyField(generic_user, related_name='events', blank=True, null=True)
     chosen_users = models.ManyToManyField(generic_user, blank=True, null=True, related_name='qualified_events')
-    logo=models.FileField(upload_to="logos/", blank=True, null=True)
-    tabs=models.ForeignKey(Tabs)
+    flagged_by = models.ManyToManyField(generic_user,blank=True, null= True, related_name='flagged_events')
+    logo = models.FileField(upload_to="logos/", blank=True, null=True)
+    tabs = models.ForeignKey(Tabs)
+    event_type = models.TextField(max_length=30) #Could be bool field also
     #Many to Many or foreign key ? I dunno
     # OR tabs=models.XMLField()
     #Defined tabs as an XML field. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
