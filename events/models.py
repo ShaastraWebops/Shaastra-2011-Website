9bce50a61c9c3a17e70f3c4fa9de5b13e2d5dd39
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-09 16:14:12 +0530

    Copied last years questions model

--- events/models.py
+++ events/models.py
@@ -19,12 +19,46 @@ class Tag(models.Model):
         pass
 
 class image_for_tabs(models.Model):
-    image=ImageField(upload_to="public_html/2011/media/") #how to avoid file conflicts ? Files with same name
+    image=ImageField(upload_to="public_html/2011/media/") #how to avoid file conflicts ? Files with same name can't be stored in same directory
     
 class Tabs(models.Model):
     content=models.TextField()
     tabimage=models.ManyToManyField(image_for_tabs)  #One tab may have multiple images. We don't know how many
     #Have yet to write questions and discussion forums classes 
+
+#This is just last years code for questions. Need to think of ways to make it better    
+class Question(models.Model):
+    TYPE = (
+            (1, 'Normal'),
+            (2, 'File'),
+            (3, 'MCQ'),
+            #(4, 'Message'),
+            )
+    
+    event = models.ForeignKey(event)
+    content = models.TextField(max_length=1000)
+    pref_id = models.IntegerField(blank= False, null = False ,verbose_name = 'Question Number. Note that this will be the number displayed in the button for this question.',)
+    type = models.IntegerField(choices=TYPE)
+    visible = models.BooleanField(default=True)
+    def __str__(self):
+        return self.content
+    def get_choices (self):
+        if self.type != 3:
+            raise ValueError
+        list = MCQOption.objects.filter(question=self)
+        choices = []
+        num = 1
+        for option in list:
+            choices.append ((num, option.content))
+            num+=1
+
+        return tuple(choices)
+
+    class Admin:
+        pass
+    
+    class Meta:
+	ordering = ['pref_id','id',]    
          
 class event(models.Model):
     
@@ -37,11 +71,11 @@ class event(models.Model):
     chosen_users = models.ManyToManyField(generic_user, blank=True, null=True, related_name='qualified_events')
     flagged_by = models.ManyToManyField(generic_user,blank=True, null= True, related_name='flagged_events')
     logo = models.FileField(upload_to="logos/", blank=True, null=True)
-    tabs = models.ForeignKey(Tabs)
     event_type = models.TextField(max_length=30) #Could be bool field also
+    tabs = models.ForeignKey(Tabs)
     #Many to Many or foreign key ? I dunno
     # OR tabs=models.XMLField()
-    #Defined tabs as an XML field. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
+    #We could define tabs as an XML field in event itself. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
     
     def __str__(self):
         return self.name
