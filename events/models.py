82a9517bf26fa287d44f74db6e8eb9296d709a34
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-09 16:19:17 +0530

    Fixed my n00b errors

--- events/models.py
+++ events/models.py
@@ -19,13 +19,37 @@ class Tag(models.Model):
         pass
 
 class image_for_tabs(models.Model):
-    image=ImageField(upload_to="public_html/2011/media/") #how to avoid file conflicts ? Files with same name can't be stored in same directory
+    image=models.ImageField(upload_to="public_html/2011/media/") #how to avoid file conflicts ? Files with same name can't be stored in same directory
     
 class Tabs(models.Model):
     content=models.TextField()
     tabimage=models.ManyToManyField(image_for_tabs)  #One tab may have multiple images. We don't know how many
     #Have yet to write questions and discussion forums classes 
 
+  
+         
+class event(models.Model):
+    
+    name = models.CharField(max_length=100)
+    url = models.URLField(null=True,verify_exists=False, blank=True)
+    start_time = models.DateTimeField(null=True,blank=True)
+    end_time = models.DateTimeField(null=True,blank=True)
+    registerable = models.BooleanField()
+    users = models.ManyToManyField(generic_user, related_name='events', blank=True, null=True)
+    chosen_users = models.ManyToManyField(generic_user, blank=True, null=True, related_name='qualified_events')
+    flagged_by = models.ManyToManyField(generic_user,blank=True, null= True, related_name='flagged_events')
+    logo = models.FileField(upload_to="logos/", blank=True, null=True)
+    event_type = models.TextField(max_length=30) #Could be bool field also
+    tabs = models.ForeignKey(Tabs)
+    #Many to Many or foreign key ? I dunno
+    # OR tabs=models.XMLField()
+    #We could define tabs as an XML field in event itself. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
+    
+    def __str__(self):
+        return self.name
+
+    class Admin:
+        pass
 #This is just last years code for questions. Need to think of ways to make it better    
 class Question(models.Model):
     TYPE = (
@@ -58,31 +82,7 @@ class Question(models.Model):
         pass
     
     class Meta:
-	ordering = ['pref_id','id',]    
-         
-class event(models.Model):
-    
-    name = models.CharField(max_length=100)
-    url = models.URLField(null=True,verify_exists=False, blank=True)
-    start_time = models.DateTimeField(null=True,blank=True)
-    end_time = models.DateTimeField(null=True,blank=True)
-    registerable = models.BooleanField()
-    users = models.ManyToManyField(generic_user, related_name='events', blank=True, null=True)
-    chosen_users = models.ManyToManyField(generic_user, blank=True, null=True, related_name='qualified_events')
-    flagged_by = models.ManyToManyField(generic_user,blank=True, null= True, related_name='flagged_events')
-    logo = models.FileField(upload_to="logos/", blank=True, null=True)
-    event_type = models.TextField(max_length=30) #Could be bool field also
-    tabs = models.ForeignKey(Tabs)
-    #Many to Many or foreign key ? I dunno
-    # OR tabs=models.XMLField()
-    #We could define tabs as an XML field in event itself. Thought it would be easier to handle an XML file. I don't know how to write to view for this yet. We need to find out 
-    
-    def __str__(self):
-        return self.name
-
-    class Admin:
-        pass
-    
+	ordering = ['pref_id','id',]      
 
 
 
