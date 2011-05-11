f847ed3f6142aa747bf999819b1487b0239f44cd
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-11 20:17:22 +0530

    Added urls.py in events

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^main_test/events/', include ('main_test.events.urls')),  
 )
 
 #Check
