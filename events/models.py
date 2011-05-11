278dcd036107dc4c75522f62ed1af754c824fefc
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-12 00:00:02 +0530

    Fixed tabs  - response to sid's mail

--- events/models.py
+++ events/models.py
@@ -14,7 +14,9 @@ class Tag(models.Model):
     
 class Tabs(models.Model): 
     # NOTE: Will one text field per tab suffice?
-    text        = models.CharField(max_length=10000)
+    title       = models.CharField(max_length=100)
+    event       = models.ForeignKey(Event)
+    text        = models.TextField() 
     #images      = models.ManyToManyField(TabImage      , blank=True, null=True, related_name='questions')
     #questions   = models.ManyToManyField(TabQuestion   , blank=True, null=True, related_name='questions')
     #forums      = models.ManyToManyField(TabForum      , blank=True, null=True, related_name='forums')
@@ -100,8 +102,6 @@ class Event(models.Model):
     logo=models.FileField(upload_to="public_html/2011/events_logos/", blank=True, null=True)
     sponslogo=models.FileField(upload_to="public_html/2011/events_sponslogos/", blank=True, null=True)
     
-    # Tabs
-    tabs = models.ManyToManyField(Tabs, blank=True, null=True, related_name='tabs')
   
     def __str__(self):
         return self.name
