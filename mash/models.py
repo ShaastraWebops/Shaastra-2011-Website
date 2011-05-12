a10a98ede8ccbdab52150fbf538fd75f4357c4db
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-12 15:55:37 +0530

    Added photois to mash

--- mash/models.py
+++ mash/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from main_test.users.models import *
 
 class photos(models.Model):
-    photolink = models.Urlfield
-    rating = models.FloatField (max_digits=7,decimal_places=2)
-    groupnum = models.IntegerField(blank=False, null=False)
-    photo= models.ImageField(upload_to='/galleryfinal')
-    user=models.Foreignkey(generic_user)
+    photolink      = models.URLField()
+    photoid        = models.AutoField(primary_key=True)
+    rating         = models.FloatField (max_digits=7,decimal_places=2)
+    groupnum       = models.IntegerField(blank=False, null=False)
+    photo          = models.ImageField(upload_to=('/galleryfinal/%s',photoid)
+    user           = models.Foreignkey(generic_user)
     
     def __str__(self):
-        return self.photolink
-
+        return str(self.photoid)
+#Since photoid is an autofield it ll automatically increment and each photo will have a unique photo id. It ll be saved to galleryfinal/[photoid].jpg . The photo can be accessed with the help of the photo id. 
 
