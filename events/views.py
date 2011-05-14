e86ef596c42506075874de8f7f5db7d1a7c850aa
Sivaramakrishnan.N.R <srk@srk-laptop.(none)>
2011-05-14 23:23:18 +0530

    gave edit a try.....

--- events/views.py
+++ events/views.py
@@ -22,3 +22,36 @@ def show_quick_tab(request):
     return render_to_response('events/QuickTabs.html', locals(), context_instance= global_context(request)) 
    
         
+def edit_content(request,event_name = None,title)
+    
+    user = request.user
+    userprof = user.get_profile()
+    events_list = models.Event.objects.filter(registerable=True)
+    
+    #invalid 
+    if e_name is None or e_name == '':  
+    
+    # to be redirected to appropriate page
+    else:
+    
+    	e_name = decamelize(e_name)   
+        e_name = e_name.replace('/','')
+        
+        if not events_list.filter(name=event_name): 
+        # to be redirected to appropriate page
+        else:
+        
+        	coord_list = models.coord.objects.filter(event_name = event_name)
+        	if not user in coord_list:
+        	#redirect to appropriate page
+        	else:
+        	
+        		if request.method == 'POST':
+        			data = request.POST.copy()
+        			form = forms.EditData(data)
+        			qtab = models.QuickTab.objects.filter(title = title, event = event_name)
+        			if form.is_valid():
+        				text = form.cleaned_data["text"]
+        				qtab.text = text
+       
+        				
