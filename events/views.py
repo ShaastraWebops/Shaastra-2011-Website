0277a3c80896592b83c3cb72293bd3aa7798c2c2
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-14 19:31:59 +0530

    view for QuickTabs

--- events/views.py
+++ events/views.py
@@ -6,3 +6,9 @@ from main_test.users.models import *
 
 import forms, models
 
+#I m _not_ writing templates write now. Just creating empty html files. 
+def show_quick_tab(request):
+    data=QuickTab.objects.all()
+    event_name,title,text=data.event.name,data.title,data.text
+    return render_to_response('events/QuickTabs.html', locals(), context_instance= global_context(request)) 
+
