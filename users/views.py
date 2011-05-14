7ad5d23aeed9a313331767718e4fdc31957bb4ff
karthikabinav <karthikabinavs@gmail.com>
2011-05-14 18:38:18 +0530

    copied the user registration from last year

--- users/views.py
+++ users/views.py
@@ -1,1 +1,55 @@
-# Create your views here.
+# just copied the user registration
+# i dont think much of changes required 
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.template.loader import get_template
+from django.template.context import Context, RequestContext
+
+import forms
+from main_test.users import models
+import sha, random, datetime
+
+def user_registration(request):
+
+	colls = models.College.objects.all()
+    
+
+	if request.method=='POST':
+		data = request.POST.copy()
+        form = forms.AddUserForm (data)
+        
+        if form.is_valid():
+            if form.cleaned_data["password"] == form.cleaned_data["password_again"]:
+                user = models.User.objects.create_user(
+                    username = form.cleaned_data['username'],
+                    email = form.cleaned_data['email'],
+                    password = form.cleaned_data['password']
+                    )
+
+				college=form.cleaned_data['college']
+
+				user.is_active = False
+
+				salt = sha.new(str(random.random())).hexdigest()[:5]
+                activation_key = sha.new(salt+user.username).hexdigest()
+                key_expires=datetime.datetime.today() + datetime.timedelta(2)
+
+				user_profile = models.generic_user(
+                        user = user,
+                        first_name = form.cleaned_data['first_name'].lower(),
+                        last_name = form.cleaned_data['last_name'].lower(),
+                        college = college,
+                        mobile_number = form.cleaned_data['mobile_number'],
+                        gender = form.cleaned_data['gender'],
+                        age = form.cleaned_data['age'],
+                        branch = clean_string(form.cleaned_data['branch']),
+                        college_roll=form.cleaned_data['college_roll'],
+                        want_hospi = form.cleaned_data['want_hospi'],
+                        activation_key = activation_key,
+                        key_expires = key_expires,
+                    )
+                user.save()
+
+                   
