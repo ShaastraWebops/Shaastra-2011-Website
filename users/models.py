a8616962e2f917b2b7f3a1785f9dee7e3c3e449c
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-17 18:00:48 +0530

    Fixed one error

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib import admin
-#from django import forms
-from main_test.events.models import Event,TeamEvent
+from django import forms
+#from main_test.events.models import Event,TeamEvent
 from django import forms
 #from events.models import Event,TeamEvent
 
