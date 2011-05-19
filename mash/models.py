596934b72b6e06ac11465782e2e130921cb2ae04
Chinmay <chinmay@Chinmay-Vostro-1015.(none)>
2011-05-19 23:36:06 +0530

    added uploader viw to mash

--- mash/models.py
+++ mash/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from main_test.users.models import *
+from django import forms
 
 class photos(models.Model):
     photolink      = models.URLField()
@@ -12,4 +13,10 @@ class photos(models.Model):
     def __str__(self):
         return str(self.photoid)
 #Since photoid is an autofield it ll automatically increment and each photo will have a unique photo id. It ll be saved to galleryfinal/[photoid].jpg . The photo can be accessed with the help of the photo id. 
+
+#This is the form for uploading
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file  = forms.FileField()
+
 
