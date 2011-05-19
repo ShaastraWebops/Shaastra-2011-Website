33c80112e4a5575fc58f7342bb539db998e15a5a
Sudharshan <sudharshan@sudharshan.(none)>
2011-05-19 23:12:54 +0530

    How many times will have this problem!

--- events/models.py
+++ events/models.py
@@ -6,14 +6,14 @@
 from django.db import models
 from django.contrib import admin
 
-<<<<<<< HEAD
+
 from django.contrib.auth.models import User, Group
 from users.models import Team
-=======
+
 #Global - Directory where all the other image directories go
 IMAGE_DIR = '2011/media/main/images/'
 FILE_DIR = '2011/media/main/files/'
->>>>>>> 42badaa363a82167fda822fdbab1ecf43cb0ff32
+
 
 # Please note that __unicode__ is not recommended in django docs. Should we switch to unicode ?
 class Tag(models.Model):   
