41a57522775efcf8d543dc763e0b8c4a2240a46f
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-20 15:20:20 +0530

    added the urls for events

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,11 @@ handler404 = "main_test.misc.util.not_found"
 handler500 = "main_test.misc.util.server_error"
 
 urlpatterns = patterns('main_test.events.views',
-      (r'^$', 'show_quick_tab'),
+      (r'^login/$', 'coordslogin'),
+      (r'^{eventname}/$', 'show_quick_tab'),
+      (r'^dashboard/$', 'dashboard'),
+      (r'^dashboard/add_tab/$', 'add_quick_tab'),
+      (r'^dashboard/edit_tab/$', 'edit_tab_content'),
+      (r'^dashboard/delete_tab/$', 'remove_tab_content'),
 )
 
