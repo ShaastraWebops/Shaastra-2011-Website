df9d48976554b3fb52b2b5ad9ce6eaba7b632f1d
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-22 00:23:30 +0530

    added main_test to events/urls.py

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from views import *
 handler404 = "main_test.misc.util.not_found"
 handler500 = "main_test.misc.util.server_error"
 
-urlpatterns = patterns('events.views',
+urlpatterns = patterns('main_test.events.views',
       (r'^login/$', 'coordslogin'),
       (r'^dashboard/$', 'dashboard'),
       (r'^dashboard/add_tab/$', 'add_quick_tab'),
