4776b37c04fb7247d63c880782e976623aba6bbb
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-22 00:17:31 +0530

    fix in models.py

--- events/models.py
+++ events/models.py
@@ -97,7 +97,7 @@ class TabFile(models.Model):
     class Admin:
         pass
 
-class TabImage(models.Model):
+"""class TabImage(models.Model):
     # TASK: Each tab can have more than one image. Each tab can be associated with more than one TabImage object(s)
     # Rename the image file to the id of the TabImage object. 
     # Ex: When a user wants to upload a image file, a TabImage object is created. Say it's id is 44. 
@@ -112,7 +112,7 @@ class TabImage(models.Model):
         return str(self.image_id)
     class Admin:
         pass
-
+"""
 class TabForumReply(models.Model):
     reply_by = models.ForeignKey(User,blank=True, null=True, related_name='reply_by')
     #We could display some profile details of the poster. Like in launchpad or bugzilla
@@ -127,7 +127,7 @@ class TabForumReply(models.Model):
     dislikes = models.IntegerField()
     # NOTE: Do we need edit history? I don't think it's worth implementing this feature.
     
-class TabForum(models.Model):
+"""class TabForum(models.Model):
     name = models.CharField( max_length = 30 )
     #Name of the thread , could be decided by the author of the thread
     tags = models.ManyToManyField(Tag, blank=True, null=True)
@@ -141,26 +141,11 @@ class TabForum(models.Model):
         return self.name
     class Admin:
         pass
-
+"""
 
 
 
 class QuickTabs(models.Model): 
-    # NOTE: Will one text field per tab suffice?
-    title  = models.CharField(max_length=80)
-    text        = models.CharField(max_length=10000)
-    events     = models.ForeignKey(Event)
-    #images      = models.ManyToManyField(TabImage      , blank=True, null=True, related_name='questions')
-    #questions   = models.ManyToManyField(TabQuestion   , blank=True, null=True, related_name='questions')
-    #forums      = models.ManyToManyField(TabForum      , blank=True, null=True, related_name='forums')
-  
-    def __str__(self):
-        return self.text
-    class Admin:
-        pass
-
-
-class Tabs(models.Model): 
     # NOTE: Will one text field per tab suffice?
     title  = models.CharField(max_length=80)
     text        = models.CharField(max_length=10000)
