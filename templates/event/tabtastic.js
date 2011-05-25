871c61f64b3f90f60117b4e6735a59cf79f88d44
karthikabinav <karthikabinavs@gmail.com>
2011-05-25 15:09:18 +0530

    clubbed all js.should work

--- templates/event/tabtastic.js
+++ templates/event/tabtastic.js
@@ -1,3 +1,116 @@
+
+//*** This code is copyright 2002-2003 by Gavin Kistner and Refinery; www.refinery.com
+//*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+//*** Reuse or modification is free provided you abide by the terms of that license.
+//*** (Including the first two lines above in your source code satisfies the conditions.)
+
+//***Adds a new class to an object, preserving existing classes
+function AddClass(obj,cName){ KillClass(obj,cName); return obj && (obj.className+=(obj.className.length>0?' ':'')+cName); }
+
+//***Removes a particular class from an object, preserving other existing classes.
+function KillClass(obj,cName){ return obj && (obj.className=obj.className.replace(new RegExp("^"+cName+"\\b\\s*|\\s*\\b"+cName+"\\b",'g'),'')); }
+
+//***Returns true if the object has the class assigned, false otherwise.
+function HasClass(obj,cName){ return (!obj || !obj.className)?false:(new RegExp("\\b"+cName+"\\b")).test(obj.className) }
+
+
+//*** This code is copyright 2002-2003 by Gavin Kistner, gavin@refinery.com
+//*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+//*** Reuse or modification is free provided you abide by the terms of that license.
+//*** (Including the first two lines above in your source code satisfies the conditions.)
+
+// Add a new stylesheet to the document;
+// url [optional] A url to an external stylesheet to use
+// idx [optional] The index in document.styleSheets to insert the new sheet before
+function AddStyleSheet(url,idx){
+	var css,before=null,head=document.getElementsByTagName("head")[0];
+
+	if (document.createElement){
+		if (url){
+			css = document.createElement('link');
+			css.rel  = 'stylesheet';
+			css.href = url;
+		} else css = document.createElement('style');
+		css.media = 'all';
+		css.type  = 'text/css';
+
+		if (idx>=0){
+			for (var i=0,ct=0,len=head.childNodes.length;i<len;i++){
+				var el = head.childNodes[i];
+				if (!el.tagName) continue;
+				var tagName = el.tagName.toLowerCase();
+				if (ct==idx){
+					before = el;
+					break;
+				}
+				if (tagName=='style' || tagName=='link' && (el.rel && el.rel.toLowerCase()=='stylesheet' || el.type && el.type.toLowerCase()=='text/css') ) ct++;
+			}
+		}
+		head.insertBefore(css,before);
+
+		return document.styleSheets[before?idx:document.styleSheets.length-1];
+	} else return alert("I can't create a new stylesheet for you. Sorry.");
+}
+// e.g. var newBlankSheetAfterAllOthers = AddStyleSheet(); 
+// e.g. var newBlankSheetBeforeAllOthers = AddStyleSheet(null,0);
+// e.g. var externalSheetAfterOthers = AddStyleSheet('http://phrogz.net/JS/Classes/docs.css');
+// e.g. var externalSheetBeforeOthers = AddStyleSheet('http://phrogz.net/JS/Classes/docs.css',0);
+
+
+// Cross-browser method for inserting a new rule into an existing stylesheet.
+// ss       - The stylesheet to stick the new rule in
+// selector - The string value to use for the rule selector
+// styles   - The string styles to use with the rule
+function AddRule(ss,selector,styles){
+	if (!ss) return false;
+	if (ss.insertRule) return ss.insertRule(selector+' {'+styles+'}',ss.cssRules.length);
+	if (ss.addRule){
+		ss.addRule(selector,styles);
+		return true;
+	}
+	return false;
+}
+
+// e.g. AddRule( document.styleSheets[0] , 'a:link' , 'color:blue; text-decoration:underline' );
+// e.g. AddRule( AddStyleSheet() , 'hr' , 'display:none' );
+
+
+//*** This code is copyright 2003 by Gavin Kistner, gavin@refinery.com
+//*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+//*** Reuse or modification is free provided you abide by the terms of that license.
+//*** (Including the first two lines above in your source code satisfies the conditions.)
+
+
+//***Cross browser attach event function. For 'evt' pass a string value with the leading "on" omitted
+//***e.g. AttachEvent(window,'load',MyFunctionNameWithoutParenthesis,false);
+
+function AttachEvent(obj,evt,fnc,useCapture){
+	if (!useCapture) useCapture=false;
+	if (obj.addEventListener){
+		obj.addEventListener(evt,fnc,useCapture);
+		return true;
+	} else if (obj.attachEvent) return obj.attachEvent("on"+evt,fnc);
+	else{
+		MyAttachEvent(obj,evt,fnc);
+		obj['on'+evt]=function(){ MyFireEvent(obj,evt) };
+	}
+} 
+
+//The following are for browsers like NS4 or IE5Mac which don't support either
+//attachEvent or addEventListener
+function MyAttachEvent(obj,evt,fnc){
+	if (!obj.myEvents) obj.myEvents={};
+	if (!obj.myEvents[evt]) obj.myEvents[evt]=[];
+	var evts = obj.myEvents[evt];
+	evts[evts.length]=fnc;
+}
+function MyFireEvent(obj,evt){
+	if (!obj || !obj.myEvents || !obj.myEvents[evt]) return;
+	var evts = obj.myEvents[evt];
+	for (var i=0,len=evts.length;i<len;i++) evts[i]();
+}
+
+
 //*** This library is copyright 2004 by Gavin Kistner, !@phrogz.net
 //*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
 //*** Reuse or modification is free provided you abide by the terms of that license.
@@ -86,7 +199,9 @@ AttachEvent(window,'load',function(){
 		if ((match=anchorMatch.exec(location.href)) && (a=everyTabThereIsById[match[1]])) SetTabActive(a);
 		
 		//Comment out the next line and include the file directly if you need IE5Mac or Opera7 support.
-		AddStyleSheet('tabtastic.css',0);
+		AddStyleSheet('{{MEDIA_URL}}tabtastic.css',0);
 	}
 	Init();
 },false);
+
+
