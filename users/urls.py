c4af8cc8ecc62d589462c01d002eb0a6c52963cd
karthikabinav <karthikabinavs@gmail.com>
2011-05-30 19:08:53 +0530

    small change in urls again

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ handler404 = "main_test.misc.util.not_found"
 handler500 = "main_test.misc.util.server_error"
 
 urlpatterns = patterns('main_test.events.views',
-      (r'^$', 'coordslogin'),
+      (r'^/$', 'coordslogin'),
     """   
       (r'^login/$', 'login'),
       (r'^login/forgot/$', 'forgot_password'),
