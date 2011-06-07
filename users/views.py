6265c0b455bccf9d8874c1acd98a6c70b083781c
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-06-08 00:15:31 +0530

    Fixed indentation errors. Habba.

--- users/views.py
+++ users/views.py
@@ -88,31 +88,25 @@ def activate (request, a_key = None ):
         try:
 	        user_profile = models.UserProfile.objects.get(activation_key = a_key)
         except ObjectDoesNotExist:
-	        prof_dne = True
-	    return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
-      
-      #Cleanup operation
-    if user_profile.key_expires < datetime.datetime.today():
-	    expired = True
-	    user = user_profile.user
-	    user.delete()
-	    user_profile.delete()
-	    return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
+            prof_dne = True
+            return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
+        if user_profile.key_expires < datetime.datetime.today():
+	        expired = True
+	        user = user_profile.user
+	        user.delete()
+	        user_profile.delete()
+	        return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
 	
-    else:
-	    user = user_profile.user
-	    user.is_active = True
-	    user.save()
-	    request.session["registered"]=True
-	
-	#send another mail
-	    mail_template=get_template('email/thankyou.html')
-        body = mail_template.render(Context({'username':user.username}))
-        send_mail('Account activated', body, 'noreply@shaastra.org', [user.email,], fail_silently=False)
-        
-	#print "IS AUTHENTICATED",user.is_authenticated()
-	    activated = True
-	    return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
+        else:
+            user = user_profile.user
+            user.is_active = True
+            user.save()
+            request.session["registered"]=True
+            mail_template=get_template('email/thankyou.html')
+            body = mail_template.render(Context({'username':user.username}))
+            send_mail('Account activated', body, 'noreply@shaastra.org', [user.email,], fail_silently=False)
+            activated = True
+            return render_to_response('registration/activated.html',locals(), context_instance= global_context(request))
 
 @needs_authentication
 def myshaastra(request):
