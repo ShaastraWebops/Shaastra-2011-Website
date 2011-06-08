7c0ed0442a41dca2318b5298a32c2b4562b138db
Chinmay <chinmay@Chinmay-Vostro-1015.(none)>
2011-06-08 15:24:32 +0530

    some changes

--- techmash/views.py
+++ techmash/views.py
@@ -27,12 +27,13 @@ def register(request):
             return HttpResponseRedirect("%stechmash/accounts/login/" % settings.SITE_URL)
     else:
         form = UserCreationForm()
-    return render_to_response("registration/register.html", {'form': form,}, locals(),context_instance= global_context(request))
+    return render_to_response("registration/register.html", Context({'form': form,}), locals(),context_instance= global_context(request))
 
 def profile(request):
     t = get_template('techmash/profile.html')
     html = t.render(Context({'username': request.user.username}))
     return HttpResponse(html, locals(),context_instance= global_context(request))
+	return render_to_response("/register.html", {'form': form,}, locals(),context_instance= global_context(request))
 
 def upload_file1(request):
     if request.method == 'POST':
