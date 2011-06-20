d6f97cace0f752db4f87df33b38f9213aea7d8cc
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-06-21 01:35:25 +0530

    Cahnign url patterns

--- users/urls.py
+++ users/urls.py
@@ -8,20 +8,17 @@ admin.autodiscover()
 handler404 = "main_test.misc.util.not_found"
 handler500 = "main_test.misc.util.server_error"
 
-urlpatterns = patterns('django.contrib.auth.views',
-(r'^password_change/$', 'password_change'),
-(r'^password_change/done/$', 'password_change_done'),
 
-)
-
-urlpatterns = patterns('main_test.users.views',  
+urlpatterns = patterns(  
         
-      (r'^register/user/?$', 'user_registration'),
-      (r'^register/college/?$', 'college_registration'),
-      (r'^myshaastra/$', 'myshaastra'),
-      (r'^register/activate/(?P<a_key>[\w]+)/?$', 'activate'),
-      (r'^login/?$', 'login'),
-      (r'^logout/?$', 'logout'),
+      (r'^register/user/?$', 'main_test.users.views.user_registration'),
+      (r'^register/college/?$', 'main_test.users.views.college_registration'),
+      (r'^myshaastra/$', 'main_test.users.views.myshaastra'),
+      (r'^register/activate/(?P<a_key>[\w]+)/?$', 'main_test.users.views.activate'),
+      (r'^login/?$', 'main_test.users.views.login'),
+      (r'^logout/?$', 'main_test.users.views.logout'),
+      (r'^password_change/$', 'django.contrib.auth.views.password_change'),
+      (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
 
 )   
 
