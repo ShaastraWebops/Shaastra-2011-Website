e666843fe757f7e4a75907c6525589d5938627bf
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-06-21 01:21:19 +0530

    Added password reset -1

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,18 @@
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
-
+import django.contrib.auth.views
 admin.autodiscover()
 
 handler404 = "main_test.misc.util.not_found"
 handler500 = "main_test.misc.util.server_error"
+
+urlpatterns = patterns('django.contrib.auth.views'
+(r'^password_reset/$', 'password_reset'),
+(r'^password_reset/done/$', 'password_reset_done'),
+(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm'),
+(r'^reset/done/$', 'password_reset_complete'),
+)
 
 urlpatterns = patterns('main_test.users.views',  
         
