e40298427091ffdc896428b50558b49c47d1db4e
Chetan Bademi <chetanbademi@gmail.com>
2011-06-27 20:21:53 +0530

    Removed the "Team" model.

--- users/models.py
+++ users/models.py
@@ -79,9 +79,3 @@ class UserProfile(models.Model):
 
     class Admin:
         pass
-
-
-class Team(models.Model):
-    name            = models.CharField(max_length=50, default = False) 
-    event           = models.ForeignKey( Event, null = False)
-    members         = models.ManyToManyField(UserProfile,null=False)
