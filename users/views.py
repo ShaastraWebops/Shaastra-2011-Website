d842a257ee51b88bec49bdad5ab02525f9ac3a6c
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-06-28 22:44:02 +0530

    Reverting to redudant code.

--- users/views.py
+++ users/views.py
@@ -65,7 +65,8 @@ def user_registration(request):
   
         if form.is_valid():
   
-            user = User.objects.create_user(username = form.cleaned_data['username'], email = form.cleaned_data['email'],password = form.cleaned_data['password'],is_active=False,)
+            user = User.objects.create_user(username = form.cleaned_data['username'], email = form.cleaned_data['email'],password = form.cleaned_data['password'],)
+            user.is_active= False
             user.save()
             salt = sha.new(str(random.random())).hexdigest()[:5]
             activation_key = sha.new(salt+user.username).hexdigest()
