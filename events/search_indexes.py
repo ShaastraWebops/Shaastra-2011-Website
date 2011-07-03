834b99564ff6e321fa46b2645e238ba8db911d9c
Sivaramakrishnan.N.R <srk@server1.example.com>
2011-07-03 18:59:47 +0530

    ;;

--- events/search_indexes.py
+++ events/search_indexes.py
@@ -2,7 +2,9 @@ from haystack.indexes import *
 from haystack import site
 from main_test.events.models import Event
 
-class EventIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
+class EventIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+    url = indexes.CharField(model_attr='url')
 
 site.register(Event, EventIndex)
