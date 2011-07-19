89756951c58ff05bf457b7dfbedfe972b6658aa2
karthikabinav <karthikabinavs@gmail.com>
2011-07-19 22:07:34 +0530

    testing the spam script

---
+++ scripts/spam.py
@@ -0,0 +1,10 @@
+
+from django.core.mail import EmailMultiAlternatives
+
+def message():
+    subject, from_email, to = 'hello', 'karthikabinavs@gmail.com', 'karthikabinavs@gmail.com'
+    text_content = 'This is a image message.'
+    html_content = '<img src = "http://www.shaastra.org/2011/media/main/img/all_logos.png"'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])# sending plain text in case they cant view html
+    msg.attach_alternative(html_content, "text/html")# the additional html content added to the content for ppl who can view html content
+    msg.send()
