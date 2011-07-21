be04677d297a171be6e48f49669e677af7a3cd1c
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-07-22 00:21:16 +0530

    changed sender address

--- users/views.py
+++ users/views.py
@@ -23,7 +23,7 @@ import sha,random,datetime
 from django.core.mail import EmailMultiAlternatives
 
 def invite (request):
-    subject, from_email, to = 'hello', 'swaroop@localhost', 'swaroop551992@gmail.com'
+    subject, from_email, to = 'hello', 'hospitality@shaastra.org', 'swaroop551992@gmail.com'
     text_content = 'This is a image message.'
     html_content = '<img src = "http://www.shaastra.org/2011/media/main/img/all_logos.png"'
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])# sending plain text in case they cant view html
