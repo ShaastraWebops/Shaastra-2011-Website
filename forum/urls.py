739c1a02d1d70579fb71a4b5b3c17443a6fe4d68
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-08-06 16:03:51 +0530

    Added main_test in some more places

--- forum/urls.py
+++ forum/urls.py
@@ -26,20 +26,20 @@ sitemap_dict = {
 }
 
 urlpatterns = patterns('',
-    url(r'^$', 'forum.views.forums_list', name='forum_index'),
+    url(r'^$', 'main_test.forum.views.forums_list', name='forum_index'),
     
     url(r'^(?P<url>(rss|atom).*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feed_dict}),
 
-    url(r'^thread/(?P<thread>[0-9]+)/$', 'forum.views.thread', name='forum_view_thread'),
-    url(r'^thread/(?P<thread>[0-9]+)/reply/$', 'forum.views.reply', name='forum_reply_thread'),
+    url(r'^thread/(?P<thread>[0-9]+)/$', 'main_test.forum.views.thread', name='forum_view_thread'),
+    url(r'^thread/(?P<thread>[0-9]+)/reply/$', 'main_test.forum.views.reply', name='forum_reply_thread'),
 
-    url(r'^subscriptions/$', 'forum.views.updatesubs', name='forum_subscriptions'),
+    url(r'^subscriptions/$', 'main_test.forum.views.updatesubs', name='forum_subscriptions'),
 
-    url(r'^(?P<slug>[-\w]+)/$', 'forum.views.forum', name='forum_thread_list'),
-    url(r'^(?P<forum>[-\w]+)/new/$', 'forum.views.newthread', name='forum_new_thread'),
+    url(r'^(?P<slug>[-\w]+)/$', 'main_test.forum.views.forum', name='forum_thread_list'),
+    url(r'^(?P<forum>[-\w]+)/new/$', 'main_test.forum.views.newthread', name='forum_new_thread'),
 
-    url(r'^([-\w/]+/)(?P<forum>[-\w]+)/new/$', 'forum.views.newthread'),
-    url(r'^([-\w/]+/)(?P<slug>[-\w]+)/$', 'forum.views.forum', name='forum_subforum_thread_list'),
+    url(r'^([-\w/]+/)(?P<forum>[-\w]+)/new/$', 'main_test.forum.views.newthread'),
+    url(r'^([-\w/]+/)(?P<slug>[-\w]+)/$', 'main_test.forum.views.forum', name='forum_subforum_thread_list'),
 
     (r'^sitemap.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': sitemap_dict}),
     (r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemap_dict}),
