306c337d357977b09196766a8d2207999590cd17
Praveen Venkatesh <praveenv253@gmail.com>
2011-08-20 22:33:28 +0530

    Passing testevent in myshaastra home

--- myshaastra/views.py
+++ myshaastra/views.py
@@ -22,7 +22,10 @@ def home(request):
             category = Menu.objects.get(event = event).parent_menu
             event.similar = category.menu_set.exclude(event = event)
         except Menu.DoesNotExist:
-            raise Http404
+            if event.name == 'testevent':
+                pass
+            else:
+                raise Http404
     ''' 
         for event in events_list:
             try:
