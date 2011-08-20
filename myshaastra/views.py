84ddce6494053e59dfb0e7ee738c3346b29a53c5
Praveen Venkatesh <praveenv253@gmail.com>
2011-08-20 22:26:59 +0530

    Check if the guy is already a part of another team for the same event

--- myshaastra/views.py
+++ myshaastra/views.py
@@ -104,6 +104,11 @@ def create_team(request, event_id = None):
     if request.method == 'POST':
         form = CreateTeamForm(request.POST)
         if form.is_valid():
+            try:
+                Team.objects.get(members__pk = request.user.id, event = form.cleaned_data['event'])
+                return render_to_response('myshaastra/already_part_of_a_team.html', locals(), context_instance = global_context(request))
+            except Team.DoesNotExist:
+                pass
             team = form.save(commit = False)
             team.leader = user
             try:
