b28a02c59dc8e909f67d4939348c052f161f58f8
Swaroop <swaroop551992@gmail.com>
2011-08-22 18:55:17 +0530

    uploadfileform error ?

--- techmash/models.py
+++ techmash/models.py
@@ -5,7 +5,7 @@ from django import forms
 import os,os.path
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
+    #title = forms.CharField(max_length=50)
     file  = forms.ImageField()
 
 class selectaphoto(forms.Form):
