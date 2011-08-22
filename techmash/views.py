03267d2e0adaef36caf4617ab2a6de1755e16ed2
Swaroop <swaroop551992@gmail.com>
2011-08-22 18:52:42 +0530

    oh no . upload still cupping

--- techmash/views.py
+++ techmash/views.py
@@ -71,8 +71,7 @@ def upload(request):
             #handle_uploaded_image(request.FILES['file'])
             return HttpResponseRedirect(("%stechmash/upload/" % settings.SITE_URL))
         else:
-            form = UploadFileForm()
-            return render_to_response('techmash/upload_file.html', locals(),context_instance= global_context(request))    
+            return HttpResponseRedirect(("%stechmash/upload/" % settings.SITE_URL))   
     else:
         form = UploadFileForm()
         return render_to_response('techmash/upload_file.html', locals(),context_instance= global_context(request))
