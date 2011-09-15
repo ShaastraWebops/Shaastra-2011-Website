a47f279253b8050cefb95c079882ad297e859841
Swaroop <swaroop551992@gmail.com>
2011-09-15 19:30:58 +0530

    Redirect to profile page

--- techslam/views.py
+++ techslam/views.py
@@ -46,7 +46,7 @@ def profile(request,username=None):
             username = request.user.username
         else:
             request.session['from_url'] = settings.SITE_URL + "techslam/myprofile/showprofile"
-            return HttpResponseRedirect(settings.SITE_URL + "login")    
+            return HttpResponseRedirect(settings.SITE_URL + "techslam/myprofile/showprofile/")    
     if username == request.user.username:
         show_buttons = True          
     try:
